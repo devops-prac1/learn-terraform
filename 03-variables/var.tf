16d4b8e65bf1507830164fc2fb6bd41382afb2b7
dspriyasg <priya9.sdr@gmail.com>
2024-03-19 10:03:10 +0800

    created instances for  expense

--- 03-variables/var.tf
+++ 03-variables/var.tf
@@ -1,5 +1,40 @@
+v# Declare the variable with value
+
 variable "x" {
   default = 100
 }
 
-variable "y" {}
+# Declare variable without value
+#variable "y" {}
+
+variable "y_list" {
+  default = [1, 2, 3]
+}
+
+variable "z_map" {
+  default = {
+    x = 10
+    y = 20
+  }
+}
+
+# Print variable
+output "x" {
+  value = var.x
+}
+
+output "y" {
+  value = var.y_list
+}
+
+output "z" {
+  value = var.z_map
+}
+
+output "y_1" {
+  value = var.y_list[1]
+}
+
+output "z_x" {
+  value = var.z_map["x"]
+}
