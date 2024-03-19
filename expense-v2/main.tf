fab2f9cf0e3f7e4317e4e7518f8f4634bff9a1ff
dspriyasg <priya9.sdr@gmail.com>
2024-03-19 16:30:06 +0800

    creating instance with loops

---
+++ expense-v2/main.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "frontend" {
+  for_each = var.instance_types
+  ami           = var.ami
+  instance_type = each.value ["instance_type"]
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "ami"  {
+  default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance_type"{
+  default = "t3.micro"
+}
+variable "security_groups" {
+  default= ["sg-0aba17f40cdbaa0f"]
+}
+
+variable "instance_types" {
+  default = {
+
+    frontend ={
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      instance_type = "t3.micro"
+    }
+    backend ={
+      instance_type = "t3.micro"
+    }
+  }
+}
