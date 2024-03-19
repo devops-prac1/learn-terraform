c33bc3ba3b714c92e113f040445ef4586ca8c16a
dspriyasg <priya9.sdr@gmail.com>
2024-03-19 16:18:10 +0800

    loops with set of values

--- 07-loops/loop.tf
+++ 07-loops/loop.tf
@@ -9,12 +9,12 @@ resource "null_resource" "test1" {
 
 variable "colors_codes" {
   default = {
-     red= 31
-     green = 32
-     yellow = 33
-     blue = 34
-     magenta =35
-     cyan= 36
+    red= 31
+    green = 32
+    yellow = 33
+    blue = 34
+    magenta =35
+    cyan= 36
 
   }
 
