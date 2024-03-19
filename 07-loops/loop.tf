1842c0c12c2d41e2c59bab477b7e10bf321bf30a
dspriyasg <priya9.sdr@gmail.com>
2024-03-19 16:15:49 +0800

    loops with set of values

--- 07-loops/loop.tf
+++ 07-loops/loop.tf
@@ -3,3 +3,19 @@
   resource "null_resource" "test" {
     count = 10
   }
+resource "null_resource" "test" {
+  for_each = var.colors_codes
+}
+
+variable "colors_codes" {
+  default = {
+     red= 31
+     green = 32
+     yellow = 33
+     blue = 34
+     magenta =35
+     cyan= 36
+
+  }
+
+}
